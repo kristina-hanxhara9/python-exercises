7c8358d87878e627b0a1afab7e33d980d34903eb
kristina-hanxhara9 <sgkhanxh@liverpool.ac.uk>
2024-10-09 16:35:35 +0100

    odd number

---
+++ one.py
@@ -0,0 +1,9 @@
+##odd one out
+
+user_input = input("Enter number here :")
+number=int(user_input)
+
+if number % 2 ==0:
+    print("Number", number,"is even")
+else:
+    print("Number",number, "is odd")
